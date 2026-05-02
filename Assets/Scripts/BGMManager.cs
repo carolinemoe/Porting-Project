f01fe53d9d03687633f0bd5302e964406d6333a1
corawang <kisumwang@gmail.com>
2026-05-02 02:15:36 -0400

    Final Update

---
+++ Assets/Scripts/BGMManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+    public static BGMManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
